d02ba0929097ce83c5a251e7b2231e0a6ab20cf3
AmadiWoldu <amadiwoldu23@gmail.com>
2026-06-12 14:32:56 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 app = FastAPI()
 
-@app.get("/")
+X = np.array([[1],[2],[3],[4]])
+y = np.array([2,4,6,8])
 
-def home():
-    return {"message" : "Hello from Docker"}
+model = LinearRegression()
+model.fit(X,y)
+
+@app.get("/predict/{x}")
+def predict(x: float):
+    pred = model.predict([[x]])[0]
+    return {"prediction": float(pred)}
